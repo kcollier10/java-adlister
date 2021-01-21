63bb8fe4b1d992f8b62ede8c53bb8aa6a7e13634
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-21 16:27:01 -0600

    guess exercise started;

---
+++ src/main/java/GuessServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="GuessServlet", urlPatterns = "/guess")
+public class GuessServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.getRequestDispatcher("/guess/guessing.jsp").forward(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int random = (int) Math.ceil(Math.random() * 5);
+        int userInput = Integer.parseInt(req.getParameter("num"));
+        if (userInput == random) {
+            res.sendRedirect("/correct");
+        } else {
+            res.sendRedirect("/incorrect");
+        }
+    }
+}
