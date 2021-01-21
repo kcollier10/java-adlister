0b9c820dca7484d86257e6e9c2fcfb0d3c93a06f
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-21 13:53:47 -0600

    updates

--- src/main/java/HomePageServlet.java
+++ src/main/java/HomePageServlet.java
@@ -1,14 +1,18 @@
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//
-//@WebServlet(name = "HomePageServlet", urlPatterns = "/home")
-//public class HomePageServlet extends HttpServlet {
-//    protected void doGet(HttpServlet req, HttpServlet res) {
-//        req.setAttribute("number", 1);
-//        req.setAttribute("title", "Home Page");
-//        req.setAttribute("isLoggedIn", true);
-//
-//        req.getRequestDispatcher("/home.jsp").forward(req, res);
-//    }
-//
-//}
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HomePageServlet", urlPatterns = "/home")
+public class HomePageServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setAttribute("number", 1);
+        req.setAttribute("title", "Home Page");
+        req.setAttribute("isLoggedIn", true);
+
+        req.getRequestDispatcher("/home.jsp").forward(req, res);
+    }
+
+}
