4986bf3612495956e07c6210f48b6de9b58f9296
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-21 16:46:04 -0600

    servlet updates

--- src/main/java/IncorrectServlet.java
+++ src/main/java/IncorrectServlet.java
@@ -1,2 +1,16 @@
-public class IncorrectServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="IncorrectServlet", urlPatterns = "/incorrect")
+public class IncorrectServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String url = req.getRequestURI();
+        req.setAttribute("url", url);
+
+        req.getRequestDispatcher("/guess/outcome.jsp").forward(req, res);
+    }
 }
