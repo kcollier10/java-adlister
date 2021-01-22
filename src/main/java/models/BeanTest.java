255d1087ba32a5cfab51818018b9ed0a2a96d842
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-22 11:39:10 -0600

    javabeans exercise updates

--- src/main/java/models/BeanTest.java
+++ src/main/java/models/BeanTest.java
@@ -1,4 +1,17 @@
 package models;
 
 public class BeanTest {
+
+    public static void main(String[] args) {
+        Album album = new Album();
+        album.setId(1L);
+        album.setName("Loose");
+        album.setArtist("Nelly Furtado");
+        album.setGenre("Pop, R&B");
+        album.setReleaseDate(2006);
+        album.setSales(52000);
+
+        System.out.println("album.getName() = " + album.getName());
+        System.out.println("album.getArtist() = " + album.getArtist());
+    }
 }
--- src/main/java/models/Quote.java
+++ src/main/java/models/Quote.java
@@ -4,6 +4,31 @@ public class Quote {
 
     public Quote() {}
 
+    private long id;
+    private Author author;
+    private String content;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
