0a3446549118ef4826cb215b7ad1ab5283ba2648
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-22 11:56:46 -0600

    exercise updates

--- src/main/java/models/BeanTest.java
+++ src/main/java/models/BeanTest.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.ArrayList;
+
 public class BeanTest {
 
     public static void main(String[] args) {
@@ -13,5 +15,31 @@ public class BeanTest {
 
         System.out.println("album.getName() = " + album.getName());
         System.out.println("album.getArtist() = " + album.getArtist());
+
+        Author author = new Author();
+        author.setId(1L);
+        author.setFirstName("John");
+        author.setLastName("Doe");
+
+        Quote firstQuote = new Quote();
+        firstQuote.setId(1L);
+        firstQuote.setAuthor(author);
+        firstQuote.setContent("If you're happy and you know it, clap your hands");
+
+        Quote secondQuote = new Quote();
+        secondQuote.setId(2L);
+        secondQuote.setAuthor(author);
+        secondQuote.setContent("Ships are safe in their harbor, but that's not what ships were built for.");
+
+        ArrayList<Quote> quotes = new ArrayList<>();
+        quotes.add(firstQuote);
+        quotes.add(secondQuote);
+
+        for( Quote q:quotes) {
+            System.out.println(q.getContent());
+            System.out.println(q.getAuthor().getFirstName());
+            System.out.println(q.getAuthor().getLastName());
+        }
     }
+
 }
