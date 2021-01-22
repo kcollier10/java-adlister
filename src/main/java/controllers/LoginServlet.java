daae6de0ea4749ca9d7bb1b72487b8644e8bd3db
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-22 16:19:14 -0600

    mvc exercises

---
+++ src/main/java/controllers/LoginServlet.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+}
---
+++ src/main/java/controllers/LogoutServlet.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name="LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet {
+}
