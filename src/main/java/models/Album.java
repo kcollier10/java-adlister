9e7563508551fec98a56dffeeda91dc87fc78959
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-22 11:28:12 -0600

    java beans exercise

---
+++ src/main/java/models/Album.java
@@ -0,0 +1,62 @@
+package models;
+
+public class Album {
+
+    public Album() {}
+
+    private long id;
+    private String artist;
+    private String name;
+    private int releaseDate;
+    private double sales;
+    private String genre;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+}
