0c7937fddbe321770790f2824b2a5b2152397b33
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-25 09:45:20 -0600

    small dao updates

--- src/main/java/daos/DaoFactory.java
+++ src/main/java/daos/DaoFactory.java
@@ -4,12 +4,11 @@ import javax.servlet.jsp.jstl.core.Config;
 
 public class DaoFactory {
     private static Ads adsDao;
-//    private static Users usersDao;
-    private static Config config = new Config();
+
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
-            adsDao = new MySQLAdsDao(config);
+            adsDao = new ListAdsDao();
         }
         return adsDao;
     }
