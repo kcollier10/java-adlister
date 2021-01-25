13ada818e61f9d53e56fa089ade689b940b14d42
Kristen Collier <kristencollier@Kristens-MacBook-Air.local>
2021-01-25 10:08:33 -0600

    sessions and cookies examples

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -14,9 +15,15 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean validAttempt = (username.equals("admin") || username.equals("user")) && password.equals("password");
 
-        if (validAttempt) {
+        HttpSession session = request.getSession();
+
+        if (validAttempt && username.equals("admin")) {
+            session.setAttribute("isAdmin", true);
+            response.sendRedirect("/admin");
+        } else if (validAttempt && username.equals("user")){
+            session.setAttribute("isAdmin", false);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
---
+++ src/main/java/ViewAdminDashboardServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="ViewAdminDashboardServlet", urlPatterns = "/admin")
+public class ViewAdminDashboardServlet extends HttpServlet {
+    @Override
+    protected void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.getRequestDispatcher("/admin.jsp").forward(req, res);
+    }
+
+}
